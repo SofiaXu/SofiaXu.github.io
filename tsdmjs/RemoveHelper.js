8fe9c4ef653ce29e31e2c7906e36cd97fee01d7c
Sofia <xuyaqi888@live.cn>
2018-11-19 02:20:26 +0800

    Create RemoveHelper.js

---
+++ tsdmjs/RemoveHelper.js
@@ -0,0 +1,24 @@
+function RemoveUseless()
+{
+    var thisHref = window.location.href;
+    if (thisHref.endsWith("forum.php"))
+    {
+        RemoveSideBar();
+    }
+    else if (thisHref.match("mod=forumdisplay"))
+    {
+        RemoveSideBar();
+        RemovvFourmDisplayAD();
+    }
+}
+
+function RemoveSideBar()
+{
+    $("div#ts_sidebar_base").remove();
+}
+
+function RemovvFourmDisplayAD()
+{
+    $("tbody#separatorline").next().remove();
+    $("[colspan='6']").parent().parent().remove();
+}

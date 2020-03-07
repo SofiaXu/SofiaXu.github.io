a0f97810aadcd2d99fbd35ef06f4d0d686a6a2ff
Aoba <xuyaqi888@live.cn>
2020-03-08 01:53:23 +0800

    Create TSDMHelper.user.js

---
+++ tsdmjs/TSDMHelper.user.js
@@ -0,0 +1,70 @@
+// ==UserScript==
+// @name         天使动漫辅助
+// @namespace    http://tampermonkey.net/
+// @version      0.1.1
+// @description  提供了一些便利的发帖和管理功能，持续更新中，需要更多功能请私信联系あおば (UID: 1639751)
+// @author       Aoba xu
+// @match        https://www.tsdm.live/*
+// @grant        none
+// ==/UserScript==
+
+window.addEventListener("load", () => {
+    'use strict';
+    // 帖子预览页面增加 PID 显示
+    if (/mod=viewthread/.test(location.href) && /tid=\d+/.test(location.href)) {
+        document.querySelectorAll("[id^=pid]").forEach((item, key, parent) => {
+            var pidLabel = document.createElement("em");
+            pidLabel.innerText = item.id.replace("pid", "PID: ");
+            item.querySelector(".authi").appendChild(pidLabel);
+        });
+    }
+
+    // 增加帖子发帖时缩放图像大小功能（仅限外链图片）
+    if (/mod=post/.test(location.href)) {
+        var box = document.createElement("div");
+        box.style.float = "left";
+        box.style.position = "relative";
+        box.style.padding = "0 3px"
+        var autoresizebtn = document.createElement("a");
+        autoresizebtn.href = "javascript:;";
+        autoresizebtn.id = "autoresize-btn";
+        autoresizebtn.title = "一键缩放图片";
+        autoresizebtn.innerText = "一键缩放图片";
+        autoresizebtn.style.background = "none";
+        autoresizebtn.style.lineHeight = "20px";
+        autoresizebtn.addEventListener("click", () => {
+            var imgUrlReg = /(?:\[img(?:\]|=\d+,\d+\])|)(http(?:s|):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+\.(?:bmp|jpg|jpeg|gif|png|tif|tiff|svg|webp)(?:[-A-Za-z0-9+&@#/%?=~_|]+|))(?:\[\/img\]|)/gi;
+            var imgCount = 0;
+            document.getElementById("e_textarea").value.replace(imgUrlReg, (all, imgUrl) => {
+                imgCount++;
+                var readImage = new Image();
+                readImage.onload = () => {
+                    var h = readImage.height;
+                    var w = readImage.width;
+                    if (w > 712) {
+                        h = Math.round(readImage.height / (w / 712));
+                        w = 712;
+                    }
+                    document.getElementById("e_textarea").value = document.getElementById("e_textarea").value.replace(all, "[img=" + w + "," + h + "]" + readImage.src + "[/img]");
+                    imgCount--;
+                    if (imgCount == 0) {
+                        alert("全部缩放完成");
+                    }
+                };
+                readImage.onerror = () => {
+                    if (readImage.src.match("https:")) {
+                        readImage.src = readImage.src.replace("https:", "http:");
+                    }
+                    else {
+                        console.log(readImage + "中的图片转换失败，可能是网络问题请重试");
+                    }
+                };
+                readImage.src = imgUrl;
+                return "";
+            });
+
+        });
+        box.appendChild(autoresizebtn);
+        document.getElementById("e_button").appendChild(box);
+    }
+});

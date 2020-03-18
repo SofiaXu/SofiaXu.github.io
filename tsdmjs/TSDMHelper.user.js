a2e9b1891f22e98a8a9d8a9403ac41d4dd561667
Aoba <xuyaqi888@live.cn>
2020-03-18 16:09:33 +0800

    Use two image services
    sm.ms work again, so we use two tab to contain two image services

--- tsdmjs/TSDMHelper.user.js
+++ tsdmjs/TSDMHelper.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         天使动漫辅助
 // @namespace    http://tampermonkey.net/
-// @version      0.1.6
+// @version      0.1.7
 // @description  提供了一些便利的发帖和管理功能，持续更新中，需要更多功能请私信联系あおば (UID: 1639751)
 // @author       Aoba xu
 // @match        https://www.tsdm.live/*
 // @grant        GM_xmlhttpRequest
+// @connect      sm.ms
 // @connect      www.52loli.top
 // @run-at       document-idle
 // ==/UserScript==
@@ -98,89 +99,8 @@
             box.style.padding = "0 3px";
             box.appendChild(autoResizeBtn);
             document.getElementById("e_button").appendChild(box);
-            var imageCtrl = document.getElementById("e_image_ctrl");
-            var btnLocal = document.getElementById("e_btn_local");
-            var btnRemote = btnLocal.cloneNode(true);
-            imageCtrl.appendChild(btnRemote);
-            btnRemote.id = 'e_btn_remote';
-            var btnRemoteLink = btnRemote.querySelector('a');
-            btnRemoteLink.setAttribute("onclick", "switchImagebutton('remote');");
-            btnRemoteLink.text = "图床上传";
-            var localPanel = document.getElementById("e_local");
-            var remotePanel = localPanel.cloneNode(true);
-            imageCtrl.parentElement.appendChild(remotePanel);
-            remotePanel.id = "e_remote";
-            remotePanel.querySelector('#imgattachbtnhidden').remove()
-            remotePanel.querySelector(".notice").innerText = "使用 www.52loli.top 图床（请注意，如长时间未插入可能是图片过大或网络拥堵）";
-            var remoteForm = remotePanel.querySelector('form');
-            remoteForm.removeAttribute("action");
-            remoteForm.querySelectorAll("input[type=hidden]").forEach(el => el.remove());
-            var remoteBtn = remotePanel.querySelector("#imguploadbtn > button");
-            remoteBtn.removeAttribute("onclick");
-            var remoteFile = remoteForm.querySelector("input[type=file]");
-            remoteFile.accept = "image/*";
-            var imageUrlPreviewTip = document.createElement("p");
-            imageUrlPreviewTip.innerText = "图片链接: ";
-            remoteForm.appendChild(imageUrlPreviewTip);
-
-            var remoteBtnSpan = remoteBtn.querySelector("span");
-            var uploadHandler = _ev => {
-                var pendingFile = remoteFile.files[0];
-                if (!pendingFile) return;
-
-                if (pendingFile.size > 5000000) {
-                    alert("图片不得超过 5M");
-                    return;
-                }
-
-                var pendingFormdata = new FormData();
-                pendingFormdata.append("image", pendingFile);
-                pendingFormdata.append("file_id", "0");
-
-                remoteBtnSpan.innerText = "上传中……";
-                GM_xmlhttpRequest({
-                    method: "POST",
-                    url: "https://www.52loli.top/api/upload",
-                    data: pendingFormdata,
-                    onload: function () {
-                        if (this.status === 200) {
-                            var uploadResponse = JSON.parse(this.responseText);
-
-                            var imageUrl = "";
-                            if (uploadResponse.code === 200) {
-                                imageUrl = uploadResponse.data.url;
-                                var readImage = new Image();
-                                readImage.onload = () => {
-                                    var imageWidth = readImage.width;
-                                    var imageHeight = readImage.height;
-
-                                    var fitWidth = 712;
-                                    if (imageWidth > fitWidth) {
-                                        imageHeight = Math.round(imageHeight / imageWidth * fitWidth);
-                                        imageWidth = fitWidth;
-                                    }
-
-                                    var imageCode = unsafeWindow.wysiwyg
-                                        ? `<img src="${imageUrl}" width="${imageWidth}" height="${imageHeight}" border=0 />`
-                                        : `[img=${imageWidth},${imageHeight}]${imageUrl}[/img]`;
-
-                                    unsafeWindow.insertText(imageCode);
-                                };
-                                readImage.src = imageUrl;
-                            }
-
-                            var imageUrlPreview = document.createElement("p");
-                            imageUrlPreview.innerText = imageUrl;
-                            remoteForm.appendChild(imageUrlPreview);
-                        } else {
-                            var uploadError = JSON.parse(this.responseText);
-                            alert("图片上传出错：" + uploadError.message);
-                        }
-                        remoteBtnSpan.innerText = "上传";
-                    }
-                });
-            };
-            remoteBtn.onclick = uploadHandler;
+            addSMMSImg();
+            addLoliImg();
         }
         else {
             var pipeLine = document.createElement("span");
@@ -191,3 +111,192 @@
         }
     }
 })();
+
+function addSMMSImg() {
+    var imageCtrl = document.getElementById("e_image_ctrl");
+    var btnLocal = document.getElementById("e_btn_local");
+    var btnRemote = btnLocal.cloneNode(true);
+    imageCtrl.appendChild(btnRemote);
+    btnRemote.id = 'e_btn_remote';
+    var btnRemoteLink = btnRemote.querySelector('a');
+    btnRemoteLink.setAttribute("onclick", "switchImagebutton('remote');");
+    btnRemoteLink.text = "sm.ms 图床上传";
+    var localPanel = document.getElementById("e_local");
+    var remotePanel = localPanel.cloneNode(true);
+    imageCtrl.parentElement.appendChild(remotePanel);
+    remotePanel.id = "e_remote";
+    remotePanel.querySelector('#imgattachbtnhidden').remove()
+    remotePanel.querySelector(".notice").innerText = "使用 sm.ms 图床（请注意，如长时间未插入可能是图片过大或网络拥堵）";
+    var remoteForm = remotePanel.querySelector('form');
+    remoteForm.removeAttribute("action");
+    remoteForm.querySelectorAll("input[type=hidden]").forEach(el => el.remove());
+    var remoteBtn = remotePanel.querySelector("#imguploadbtn > button");
+    remoteBtn.removeAttribute("onclick");
+    var remoteFile = remoteForm.querySelector("input[type=file]");
+    remoteFile.accept = "image/*";
+    var imageUrlPreviewTip = document.createElement("p");
+    imageUrlPreviewTip.innerText = "图片链接: ";
+    remoteForm.appendChild(imageUrlPreviewTip);
+
+    var remoteBtnSpan = remoteBtn.querySelector("span");
+    var uploadHandler = _ev => {
+        var pendingFile = remoteFile.files[0];
+        if (!pendingFile) return;
+
+        if (pendingFile.size > 5000000) {
+            alert("图片不得超过 5M");
+            return;
+        }
+
+        var pendingFormdata = new FormData();
+        pendingFormdata.append("smfile", pendingFile);
+        pendingFormdata.append("file_id", "0");
+
+        remoteBtnSpan.innerText = "上传中……";
+        GM_xmlhttpRequest({
+            method: "POST",
+            url: "https://sm.ms/api/v2/upload?inajax=1",
+            data: pendingFormdata,
+            onload: function () {
+                if (this.status === 200) {
+                    var uploadResponse = JSON.parse(this.responseText);
+
+                    var imageUrl = "";
+                    if (uploadResponse.code === "success") {
+                        imageUrl = uploadResponse.data.url;
+                        var imageWidth = uploadResponse.data.width;
+                        var imageHeight = uploadResponse.data.height;
+
+                        var fitWidth = 712;
+                        if (imageWidth > fitWidth) {
+                            imageHeight = Math.round(imageHeight / imageWidth * fitWidth);
+                            imageWidth = fitWidth;
+                        }
+
+                        var imageCode = unsafeWindow.wysiwyg
+                            ? `<img src="${imageUrl}" width="${imageWidth}" height="${imageHeight}" border=0 />`
+                            : `[img=${imageWidth},${imageHeight}]${imageUrl}[/img]`;
+
+                        unsafeWindow.insertText(imageCode);
+                    } else if (uploadResponse.code === "image_repeated") {
+                        imageUrl = uploadResponse.images;
+                        var readImage = new Image();
+                        readImage.onload = () => {
+                            var imageWidth = readImage.width;
+                            var imageHeight = readImage.height;
+
+                            var fitWidth = 712;
+                            if (imageWidth > fitWidth) {
+                                imageHeight = Math.round(imageHeight / imageWidth * fitWidth);
+                                imageWidth = fitWidth;
+                            }
+
+                            var imageCode = unsafeWindow.wysiwyg
+                                ? `<img src="${imageUrl}" width="${imageWidth}" height="${imageHeight}" border=0 />`
+                                : `[img=${imageWidth},${imageHeight}]${imageUrl}[/img]`;
+
+                            unsafeWindow.insertText(imageCode);
+                        };
+                        readImage.src = imageUrl;
+                    }
+
+                    var imageUrlPreview = document.createElement("p");
+                    imageUrlPreview.innerText = imageUrl;
+                    remoteForm.appendChild(imageUrlPreview);
+                } else {
+                    var uploadError = JSON.parse(this.responseText);
+                    alert("图片上传出错：" + uploadError.message);
+                }
+                remoteBtnSpan.innerText = "上传";
+            }
+        });
+    };
+    remoteBtn.onclick = uploadHandler;
+}
+
+function addLoliImg() {
+    document.getElementById("e_button").appendChild(box);
+    var imageCtrl = document.getElementById("e_image_ctrl");
+    var btnLocal = document.getElementById("e_btn_local");
+    var btnRemote = btnLocal.cloneNode(true);
+    imageCtrl.appendChild(btnRemote);
+    btnRemote.id = 'e_btn_remote';
+    var btnRemoteLink = btnRemote.querySelector('a');
+    btnRemoteLink.setAttribute("onclick", "switchImagebutton('remote');");
+    btnRemoteLink.text = "52loli 图床上传";
+    var localPanel = document.getElementById("e_local");
+    var remotePanel = localPanel.cloneNode(true);
+    imageCtrl.parentElement.appendChild(remotePanel);
+    remotePanel.id = "e_remote";
+    remotePanel.querySelector('#imgattachbtnhidden').remove()
+    remotePanel.querySelector(".notice").innerText = "使用 www.52loli.top 图床（请注意，如长时间未插入可能是图片过大或网络拥堵）";
+    var remoteForm = remotePanel.querySelector('form');
+    remoteForm.removeAttribute("action");
+    remoteForm.querySelectorAll("input[type=hidden]").forEach(el => el.remove());
+    var remoteBtn = remotePanel.querySelector("#imguploadbtn > button");
+    remoteBtn.removeAttribute("onclick");
+    var remoteFile = remoteForm.querySelector("input[type=file]");
+    remoteFile.accept = "image/*";
+    var imageUrlPreviewTip = document.createElement("p");
+    imageUrlPreviewTip.innerText = "图片链接: ";
+    remoteForm.appendChild(imageUrlPreviewTip);
+
+    var remoteBtnSpan = remoteBtn.querySelector("span");
+    var uploadHandler = _ev => {
+        var pendingFile = remoteFile.files[0];
+        if (!pendingFile) return;
+
+        if (pendingFile.size > 5000000) {
+            alert("图片不得超过 5M");
+            return;
+        }
+
+        var pendingFormdata = new FormData();
+        pendingFormdata.append("image", pendingFile);
+        pendingFormdata.append("file_id", "0");
+
+        remoteBtnSpan.innerText = "上传中……";
+        GM_xmlhttpRequest({
+            method: "POST",
+            url: "https://www.52loli.top/api/upload",
+            data: pendingFormdata,
+            onload: function () {
+                if (this.status === 200) {
+                    var uploadResponse = JSON.parse(this.responseText);
+
+                    var imageUrl = "";
+                    if (uploadResponse.code === 200) {
+                        imageUrl = uploadResponse.data.url;
+                        var readImage = new Image();
+                        readImage.onload = () => {
+                            var imageWidth = readImage.width;
+                            var imageHeight = readImage.height;
+
+                            var fitWidth = 712;
+                            if (imageWidth > fitWidth) {
+                                imageHeight = Math.round(imageHeight / imageWidth * fitWidth);
+                                imageWidth = fitWidth;
+                            }
+
+                            var imageCode = unsafeWindow.wysiwyg
+                                ? `<img src="${imageUrl}" width="${imageWidth}" height="${imageHeight}" border=0 />`
+                                : `[img=${imageWidth},${imageHeight}]${imageUrl}[/img]`;
+
+                            unsafeWindow.insertText(imageCode);
+                        };
+                        readImage.src = imageUrl;
+                    }
+
+                    var imageUrlPreview = document.createElement("p");
+                    imageUrlPreview.innerText = imageUrl;
+                    remoteForm.appendChild(imageUrlPreview);
+                } else {
+                    var uploadError = JSON.parse(this.responseText);
+                    alert("图片上传出错：" + uploadError.message);
+                }
+                remoteBtnSpan.innerText = "上传";
+            }
+        });
+    };
+    remoteBtn.onclick = uploadHandler;
+}
